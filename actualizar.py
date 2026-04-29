a2268c6951e006c8d4a36effa5f8059555538fd8
kayleemontero30-ui <kayleemontero30@gmail.com>
2026-04-29 20:39:02 +0200

    ya en casa

--- actualizar.py
+++ actualizar.py
@@ -4,11 +4,12 @@ conexion = sqlite3.connect("mi_base.db")
 
 cursor = conexion.cursor()
 
+#en la base de datos se usa el lenguaje SQL para insertar datos, consultar, actualizar o eliminar registros.
 cursor.execute("""
-    UPDATE usuarios
-    SET edad = 3
-    WHERE nombre = 'Juan Pérez'
-
+    UPDATE usuarios 
+    SET edad = 40
+    WHERE id = 4      
 """)
+
 
 conexion.commit()
